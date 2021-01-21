27401cc68dcd2405323c1d2680f4cd8ac8f01dfc
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-20 23:21:50 -0700

    Add constructor & prototype method for Book, Generate DOM elements from library

--- library.js
+++ library.js
@@ -1,28 +1,69 @@
-const library = [];
+// I'm not sure if it would be better to use a map or an array
+// map: instant access if I want a specific book, & quick to remove
+// array: simpler, ?...
+const library = new Map();
 
-function Book(title, author, pageCount, isRead, rating) {
+function Book(title, author) {
   this.title = title;
   this.author = author;
-  this.pageCount = pageCount;
-  this.isRead = isRead;
-  this.rating = rating;
-
-  this.info = function() {
-    return `${title} by ${author}, ${pageCount} pages, ` +
-        `${isRead ? 'read':'not read yet'}, rating: ${rating} stars.`;
-  }
+}
+Book.prototype.info = function() {
+  return `${this.title} by ${this.author}`;
 }
 
+// !TEMP FOR TESTING!
+const testBooks = [
+  new Book('The Hobbit', 'Gandalf', 67, false, 0),
+  new Book('The Martian', 'Weir', 310, true, 4),
+  new Book('Pragmatic Programmer', 'Idk', 270, false, 3),
+  new Book('Clean Code', 'Avuncular Robert', 400, false, 2),
+  new Book('Name of the Wind', 'Pat Rothfuss', 800, true, 4),
+  new Book('LOTR', 'JRR Tolkein', 600, true, 4),
+];
+
+testBooks.forEach(book => library.set(book.title, book));
+// !ENDTEMP!
+
 function addBookToLibrary() {
-  const constructorInputs = [];
-  
   // Take input for each constructor field & push to inputs array
-  
+  const constructorInputs = getNewBookPrompt();
+
   if (Book.length === constructorInputs.length) {
-    libary.add(new Book(...constructorInputs));
+    const newBook = new Book(...constructorInputs);
+    library.set(newBook.title, newBook);
     return true;
   } else {
     // Throw an error
     return false;
   }
+}
+
+function getNewBookPrompt() {
+  const title = prompt('What is the book\'s title?');
+  const author = prompt('Who is the book\'s author?');
+  return [title, author];
+}
+
+// * DOM
+
+// Element generator
+function bookElement(book) {
+  const bookElement = document.createElement('div');
+  
+  const titleElement = document.createElement('div');
+  titleElement.textContent = `Title: ${book.title}`;
+  bookElement.appendChild(titleElement);
+  const authorElement = document.createElement('div');
+  authorElement.textContent = `Author: ${book.author}`;
+  bookElement.appendChild(authorElement);
+
+  bookElement.classList.add('book');
+
+  return bookElement;
+}
+
+bookContainer = document.querySelector('.library');
+
+function addLibraryToDOM() {
+  library.forEach(book => bookContainer.appendChild(bookElement(book)));
 }
