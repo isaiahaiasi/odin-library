689e3189f8fd60e57dfaaa918f46719efc37c742
isaiahaiasi <isaiah.althouse@gmail.com>
2021-01-21 01:59:02 -0700

    Add modal form and generate in JavaScript (not functional)

--- library.js
+++ library.js
@@ -8,7 +8,6 @@ function Book(title, author, pageCount, isRead, rating) {
   this.author = author;
   this.pageCount = pageCount;
   this.isRead = isRead;
-  this.rating = rating;
 }
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}`;
@@ -54,9 +53,22 @@ function getNewBookPrompt() {
 
 // * DOM
 const bookContainer = document.querySelector('.library');
+const modalForm = document.querySelector('.modal').querySelector('form');
+
+// Add any generated HTML to the page (ie, the form fields...)
+function initPage() {
+  modalForm.appendChild(formItem('Title:','title','text',true));
+  modalForm.appendChild(formItem('Author:','author','text',true));
+  modalForm.appendChild(formItem('Page count:','pageCount','number',true));
+  modalForm.appendChild(formItem('Have read:','isRead','checkbox',true));
+  modalForm.appendChild(formItem('Have read:','isRead','checkbox',true));
+  modalForm.appendChild(formItem('','Add book!','submit',false));
+}
 
 // Add Event Listeners
-document.querySelector('.add-book').addEventListener('click', addBookToLibrary);
+document.querySelector('.add-book').addEventListener('click', () => {
+  modalForm.style.display === 'none' ? 'block' : 'none';
+});
 
 // Element generators
 function bookElement(book) {
@@ -69,6 +81,7 @@ function bookElement(book) {
   });
 
   bookElm.classList.add('book');
+  bookElm.classList.add('card');
   bookElm.setAttribute('data-booktitle',book.title);
 
   return bookElm;
@@ -79,7 +92,34 @@ function bookPropertyElement(book, property) {
   bookPropertyElm.textContent = book[property];
   bookPropertyElm.classList.add('book-prop');
   bookPropertyElm.classList.add(`book-${property}`);
+
   return bookPropertyElm;
+}
+
+// TODO: For the modal, I need to figure out how to hide it & cancel the add
+// TODO:  if the user clicks anywhere that ISN'T the modal card...
+function formItem(label, name, type, isRequired) {
+  const newFormItem = document.createElement('div');
+  newFormItem.classList.add('form-item');
+
+  const formLabel = document.createElement('label');
+  formLabel.textContent = label;
+  formLabel.setAttribute('for', name);
+
+  const formInput = document.createElement('input');
+  formInput.setAttribute('type', type);
+  formInput.setAttribute('name', name);
+  formInput.setAttribute('id', `form-${name}`);
+  formInput.required = isRequired;
+
+  if (type === 'submit') {
+    formInput.value=name;
+  }
+
+  newFormItem.appendChild(formLabel);
+  newFormItem.appendChild(formInput);
+
+  return newFormItem;
 }
 
 // I don't want to be calling DOM methods directly from 'normal' JS,
@@ -91,3 +131,5 @@ function addLibraryToDOM() {
 function addBookToDom(book) {
   bookContainer.appendChild(bookElement(book));
 }
+
+initPage();
