7729c2395d341352bc976dee6855787fd362ac0d
Isaiah <computerblender@gmail.com>
2021-04-25 15:33:38 -0700

    change publicPath in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "/odin-library/",
   },
   module: {
     rules: [
