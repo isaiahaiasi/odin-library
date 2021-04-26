117e4786fa77b13dca4b313a36dbff594ba05dd8
Isaiah <computerblender@gmail.com>
2021-04-26 01:34:45 -0700

    Set up firebase and begin implementing auth

--- src/scripts/library.js
+++ src/scripts/library.js
@@ -3,11 +3,42 @@ import "../styles/style.css";
 
 import firebase from "firebase/app";
 import firebaseConfig from "./firebase-config";
+import "firebase/firebase-firestore";
+import "firebase/firebase-auth";
 
 import Book from "./book";
 
 firebase.initializeApp(firebaseConfig);
 
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const signedInSection = document.querySelector("#when-signed-out");
+const signedOutSection = document.querySelector("#when-signed-in");
+const signInBtn = document.querySelector("#sign-in-btn");
+const signOutBtn = document.querySelector("#sign-out-btn");
+
+signInBtn.addEventListener("click", () => {
+  auth.signInWithPopup(provider);
+});
+
+signOutBtn.addEventListener("click", () => {
+  auth.signOut();
+});
+
+const userNameElement = document.querySelector("#user-name");
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    signedInSection.hidden = false;
+    signedOutSection.hidden = true;
+    userNameElement.textContent = user.displayName;
+  } else {
+    signedInSection.hidden = false;
+    signedOutSection.hidden = true;
+    userNameElement.textContent = "no signed in user";
+  }
+});
 const library = [];
 
 const bookCntr = document.querySelector(".library");
@@ -283,3 +314,5 @@ export default function initializeLibrary() {
   loadLibrary();
   updateFooterData();
 }
+
+initializeLibrary();
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require("path");
 module.exports = {
   mode: "development",
   devtool: "inline-source-map", // fix for webpack-dev-server error in console
-  entry: "./src/scripts/index.js",
+  entry: "./src/scripts/library.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/odin-library/",
+    // publicPath: "/odin-library/", // only for gh-pages
   },
   module: {
     rules: [
