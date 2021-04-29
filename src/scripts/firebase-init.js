b5588b8aec83b7c6b3281730de56b4bbbc27c481
Isaiah <computerblender@gmail.com>
2021-04-28 20:36:24 -0700

    Begin firestore factory to replace Library/Book

--- src/scripts/firebase-init.js
+++ src/scripts/firebase-init.js
@@ -3,91 +3,98 @@ import firebaseConfig from "./firebase-config";
 import "firebase/firebase-firestore";
 import "firebase/firebase-auth";
 
-import addHandlerToAddBookButton from "./library";
+export default function firebaseHandler(library) {
+  firebase.initializeApp(firebaseConfig);
+  // * AUTH ---------------------------------------
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.initializeApp(firebaseConfig);
+  const signedInSection = document.querySelector("#when-signed-out");
+  const signedOutSection = document.querySelector("#when-signed-in");
+  const signInBtn = document.querySelector("#sign-in-btn");
+  const signOutBtn = document.querySelector("#sign-out-btn");
 
-// * AUTH ---------------------------------------
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
-
-const signedInSection = document.querySelector("#when-signed-out");
-const signedOutSection = document.querySelector("#when-signed-in");
-const signInBtn = document.querySelector("#sign-in-btn");
-const signOutBtn = document.querySelector("#sign-out-btn");
-
-signInBtn.addEventListener("click", () => {
-  auth.signInWithPopup(provider);
-});
-
-signOutBtn.addEventListener("click", () => {
-  auth.signOut();
-});
-
-const userNameElement = document.querySelector("#user-name");
-const userIdElement = document.querySelector("#user-id");
-
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    signedInSection.classList.add("hidden");
-    signedOutSection.classList.remove("hidden");
-    userNameElement.textContent = user.displayName;
-    userIdElement.textContent = user.uid;
-  } else {
-    signedOutSection.classList.add("hidden");
-    signedInSection.classList.remove("hidden");
-    userNameElement.textContent = "no signed in user";
-    userIdElement.textContent = "N/A";
-  }
-});
-
-// * FIRESTORE ---------------------------------------
-
-const db = firebase.firestore();
-
-// need a way to listen for when the "add book" button is created,
-// and then handle that here
-
-// TODO: fetch books from database
-// function getUsersBooks((user, libraryRef) => {
-
-// });
-
-auth.onAuthStateChanged(handleDatabaseStuff);
-
-function handleDatabaseStuff(user) {
-  if (!user) {
-    console.log("no user!");
-    return;
-  }
-
-  const libraryRef = db.collection(`users/${user.uid}/library`);
-
-  addHandlerToAddBookButton((bookData) => {
-    handleAddBookToDatabase(bookData, libraryRef, user);
+  signInBtn.addEventListener("click", () => {
+    auth.signInWithPopup(provider);
   });
 
-  // TODO: function to render database out
-  libraryRef
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    })
-    .catch((error) => {
-      console.log("Error getting document:", error);
+  signOutBtn.addEventListener("click", () => {
+    auth.signOut();
+  });
+
+  const userNameElement = document.querySelector("#user-name");
+  const userIdElement = document.querySelector("#user-id");
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      signedInSection.classList.add("hidden");
+      signedOutSection.classList.remove("hidden");
+      userNameElement.textContent = user.displayName;
+      userIdElement.textContent = user.uid;
+    } else {
+      signedOutSection.classList.add("hidden");
+      signedInSection.classList.remove("hidden");
+      userNameElement.textContent = "no signed in user";
+      userIdElement.textContent = "N/A";
+    }
+  });
+
+  // * FIRESTORE ---------------------------------------
+
+  const db = firebase.firestore();
+
+  // need a way to listen for when the "add book" button is created,
+  // and then handle that here
+
+  // TODO: fetch books from database
+  // function getUsersBooks((user, libraryRef) => {
+
+  // });
+
+  auth.onAuthStateChanged(handleDatabaseStuff);
+
+  async function handleDatabaseStuff(user) {
+    if (!user) {
+      console.log("no user!");
+      return;
+    }
+
+    await db.collection("users").doc(user.uid).set({}, { merge: true });
+
+    const libraryRef = db.collection(`users/${user.uid}/books`);
+
+    // TODO: function to render database out
+    // TODO: replace callbacks w firebase db listener...
+    const librarySnapshot = await libraryRef.get();
+
+    console.log("Document data:", librarySnapshot.docs);
+
+    library.onAddBook((bookData) => {
+      addBookToDatabase(bookData, libraryRef);
     });
-}
 
-function handleAddBookToDatabase(bookData, libraryRef) {
-  const { serverTimestamp } = firebase.firestore.FieldValue;
+    library.onDeleteBook((bookId) => {
+      removeBookFromDatabase(bookId, libraryRef);
+    });
+  }
 
-  libraryRef.add({
-    addedTimestamp: serverTimestamp(),
-    ...bookData,
-  });
+  function addBookToDatabase(bookData, libraryRef) {
+    const { serverTimestamp } = firebase.firestore.FieldValue;
+
+    libraryRef.add({
+      addedTimestamp: serverTimestamp(),
+      ...bookData,
+    });
+  }
+
+  function removeBookFromDatabase(bookId, libraryRef) {
+    libraryRef
+      .doc(bookId)
+      .then(() => {
+        console.log(`Book ${bookId} removed from database!`);
+      })
+      .catch(() => {
+        console.log("Error removing book from database!");
+      });
+  }
 }
